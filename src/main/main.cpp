93fb0ca360821d9c8dc122e980d50dbcb46cd65c
Richard Neumann <mail@richard-neumann.de>
2021-05-08 02:27:11 +0200

    Remove unused includes

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,13 +1,3 @@
-#include <iostream>
-using std::cout;
-using std::endl;
-
-#include <vector>
-using std::vector;
-
-#include "HitPoint.h"
-using models::HitPoint;
-
 #include "game.h"
 using game::spawn;
 
