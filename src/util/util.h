f68a50ac06bd92280e6b1c91041dc5201d846c8b
Richard Neumann <mail@richard-neumann.de>
2021-06-01 21:32:04 +0200

    Remove using statements from header file

--- src/util/util.h
+++ src/util/util.h
@@ -8,8 +8,6 @@
 #include <vector>
 
 #include <boost/program_options.hpp>
-using boost::program_options::options_description;
-using boost::program_options::variables_map;
 
 namespace util {
     std::vector<std::string> splitString(std::string const &str, std::string const &delimiter);
@@ -19,7 +17,8 @@ namespace util {
     bool isExitCommand(std::string const &command);
     bool isNumber(std::string const &str);
     int copyString(char *dest, std::string const &src, size_t size);
-    std::optional<variables_map> parseArgDesc(int argc, char const *argv[], options_description const &desc);
+    std::optional<boost::program_options::variables_map>
+        parseArgDesc(int argc, char const *argv[], boost::program_options::options_description const &desc);
 
     template <typename ElementType>
     bool contains(std::vector<ElementType> const &haystack, ElementType const &needle)
