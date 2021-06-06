e563f455e4cd54890fd2190ad56601ebb4827c50
Richard Neumann <mail@richard-neumann.de>
2021-06-06 21:23:25 +0200

    Refactor Matrix

--- src/models/Matrix.h
+++ src/models/Matrix.h
@@ -11,16 +11,17 @@
 namespace models {
     template <typename ElementType>
     class Matrix : public std::vector<std::vector<ElementType>> {
-    public:
+    private:
         using std::vector<std::vector<ElementType>>::at;
-
+        using std::vector<std::vector<ElementType>>::push_back;
+    public:
         Matrix(size_t width, size_t height, ElementType const &initializer)
         {
             if (!width || ! height)
                 throw std::invalid_argument("Matrix size must be > 0.");
 
             for (size_t y = 0; y < height; ++y)
-                this->push_back(std::vector<ElementType>(width, initializer));
+                push_back(std::vector<ElementType>(width, initializer));
         }
 
         ElementType const & at(Coordinate const &coordinate) const
