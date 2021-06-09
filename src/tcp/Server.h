050272df28bf69576735ee89cf1514ad90e3cbd3
Richard Neumann <mail@richard-neumann.de>
2021-06-10 00:36:39 +0200

    Remove unnecessary comment

--- src/tcp/Server.h
+++ src/tcp/Server.h
@@ -15,10 +15,6 @@ namespace tcp {
     private:
         boost::asio::ip::tcp::acceptor getAcceptor() const;
     protected:
-        /*
-         * Abstract method to handle requests.
-         * Must be overridden by subclasses.
-         */
         [[nodiscard]] virtual std::string handleRequest(std::string const &buf) const = 0;
     public:
         using Service::Service;
